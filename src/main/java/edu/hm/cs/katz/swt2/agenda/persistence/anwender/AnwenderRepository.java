7b1e70c786f10bb7359ed690e115e7b3f17c6a71
Bastian Katz <bastian.katz@hm.edu>
2020-04-18 23:57:39 +0200

    Dokumentation Persistence/Anwender.

--- src/main/java/edu/hm/cs/katz/swt2/agenda/persistence/anwender/AnwenderRepository.java
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/persistence/anwender/AnwenderRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository zum Zugriff auf gespeicherte Anwenderdaten.
+ * Repository zum Zugriff auf gespeicherte Anwenderdaten. Repostory-Interfaces erben eine
+ * unglaubliche Menge hilfreicher Methoden. Weitere Methoden kann man einfach durch Benennung
+ * definierern. Spring Data ergänzt die Implementierungen zur Laufzeit.
  * 
  * @author Bastian Katz (mailto: bastian.katz@hm.edu)
  */
