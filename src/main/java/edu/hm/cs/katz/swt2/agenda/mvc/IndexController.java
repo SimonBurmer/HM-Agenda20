6aa7428f03c213f39e1224a7e1ca7cabe4e23f4a
ludwig <ludwig@airhorn.intern>
2020-06-05 15:59:03 +0200

    [#37] Erstelle Aufgabe 19 B "Eingabe eines Abo-Codes"

--- src/main/java/edu/hm/cs/katz/swt2/agenda/mvc/IndexController.java
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/mvc/IndexController.java
@@ -1,14 +1,21 @@
 package edu.hm.cs.katz.swt2.agenda.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.hm.cs.katz.swt2.agenda.service.TopicService;
 
 /**
- * Controller-Klasse für die Landing-Page. Controller reagieren auf Aufrufe von URLs. Sie benennen
- * ein View-Template (Thymeleaf-Vorlage) und stellen Daten zusammen, die darin dargestellt werden.
- * Dafür verwenden Sie Methoden der Service-Schicht.
+ * Controller-Klasse für die Landing-Page. Controller reagieren auf Aufrufe von
+ * URLs. Sie benennen ein View-Template (Thymeleaf-Vorlage) und stellen Daten
+ * zusammen, die darin dargestellt werden. Dafür verwenden Sie Methoden der
+ * Service-Schicht.
  * 
  * @author Bastian Katz (mailto: bastian.katz@hm.edu)
  */
@@ -16,11 +23,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController extends AbstractController {
 
-  /**
-   * Erstellt die Landing-Page.
-   */
-  @GetMapping("/")
-  public String getIndexView(Model model, Authentication auth) {
-    return "index";
-  }
+	@Autowired
+	TopicService topicService;
+
+	/**
+	 * Erstellt die Landing-Page.
+	 */
+	@GetMapping("/")
+	public String getIndexView(Model model, Authentication auth) {
+		model.addAttribute("registration", new Registration());
+		return "index";
+	}
+
+	@PostMapping("/register")
+	public String handleRegistrationKey(@ModelAttribute("registration") Registration registration, RedirectAttributes redirectAttributes) {
+		String uuid = "";
+		String key = registration.getKey();
+		try {
+			uuid = topicService.getTopicUuid(key);
+			redirectAttributes.addFlashAttribute("sucsess", "Topic gefunden!");
+		} catch (Exception e) {
+			redirectAttributes.addFlashAttribute("error", e.getMessage());
+			return "redirect:/";
+		}
+
+		return "redirect:/topics/" + uuid + "/register";
+	}
 }
--- src/main/java/edu/hm/cs/katz/swt2/agenda/service/dto/OwnerTopicDto.java
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/service/dto/OwnerTopicDto.java
@@ -4,31 +4,36 @@ import java.util.Collection;
 import edu.hm.cs.katz.swt2.agenda.persistence.User;
 
 /**
- * Transferobjekt für Topics mit Metadaten, die nur für Verwalter eines Topics (d.h. Eigentümer des
- * Topics) sichtbar sind. Transferobjekte sind Schnittstellenobjekte der Geschäftslogik; Sie sind
- * nicht Teil des Modells, so dass Änderungen an den Transferobjekten die Überprüfungen der
- * Geschäftslogik nicht umgehen können.
+ * Transferobjekt für Topics mit Metadaten, die nur für Verwalter eines Topics
+ * (d.h. Eigentümer des Topics) sichtbar sind. Transferobjekte sind
+ * Schnittstellenobjekte der Geschäftslogik; Sie sind nicht Teil des Modells, so
+ * dass Änderungen an den Transferobjekten die Überprüfungen der Geschäftslogik
+ * nicht umgehen können.
  * 
  * @see SubscriberTopicDto
  * 
  * @author Bastian Katz (mailto: bastian.katz@hm.edu)
  */
 public class OwnerTopicDto extends SubscriberTopicDto {
-  
-  private Collection<User> subscriber;
 
-  public OwnerTopicDto(String uuid, UserDisplayDto user, String title,String shortDescription, String longDescription, Collection<User> subscriber) {
-    super(uuid, user, title, shortDescription, longDescription);
-    this.subscriber = subscriber;
-  }
+	private Collection<User> subscriber;
 
-  public Collection<User> getSubscriber() {
-    return subscriber;
-  }
+	public OwnerTopicDto(String uuid, UserDisplayDto user, String title, String shortDescription,
+			String longDescription, Collection<User> subscriber) {
+		super(uuid, user, title, shortDescription, longDescription);
+		this.subscriber = subscriber;
+	}
 
-  public void setSubscriber(Collection<User> subscriber) {
-    this.subscriber = subscriber;
-  }
-  
-  
+	public String getKey() {
+		return getUuid().substring(28);
+	}
+
+	public Collection<User> getSubscriber() {
+		return subscriber;
+	}
+
+	public void setSubscriber(Collection<User> subscriber) {
+		this.subscriber = subscriber;
+	}
+
 }
