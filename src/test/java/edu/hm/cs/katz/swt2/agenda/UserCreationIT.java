bb3dd64bddb6e77c95125f7d9936d5bad0e57c4c
ludwig <ludwig@airhorn.intern>
2020-06-11 14:26:49 +0200

    [#44] Erstelle Aufgabe 27 "Integrationstests schreiben"

---
+++ src/test/java/edu/hm/cs/katz/swt2/agenda/UserCreationIT.java
@@ -0,0 +1,50 @@
+package edu.hm.cs.katz.swt2.agenda;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.transaction.Transactional;
+import javax.validation.ValidationException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import edu.hm.cs.katz.swt2.agenda.service.UserService;
+
+@SpringBootTest
+@ActiveProfiles("none")
+@Transactional
+
+public class UserCreationIT {
+
+	@Autowired
+	UserService userService;
+
+	@Test
+	@WithUserDetails("admin")
+	public void createdUserContainsAllInformation() {
+
+		userService.legeAn("tiffy", "Tiffy", "Tiffy123*", false);
+
+		var createdUser = userService.getUserInfo("tiffy");
+
+		assertEquals("tiffy", createdUser.getLogin());
+		assertEquals("Tiffy", createdUser.getName());
+		assertEquals("Tiffy", createdUser.getDisplayName());
+	}
+
+	@Test
+	@WithUserDetails("admin")
+	public void cannotCreateExistingUser() {
+		
+		userService.legeAn("tiffy", "Tiffy", "Tiffy123*", false);
+
+		assertThrows(ValidationException.class, () -> {
+			userService.legeAn("tiffy", "Tiffy", "Tiffy123*", false);
+		});
+
+	}
+
+}
