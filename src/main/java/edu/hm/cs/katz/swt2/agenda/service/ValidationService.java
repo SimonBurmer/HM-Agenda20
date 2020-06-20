653c924bd6b7b21778cb4253c9b8dc7abd2c781d
Simon <Simon_burmer@yahoo.de>
2020-06-20 08:37:33 +0200

    [#42] Verbesserung der Validation.

--- src/main/java/edu/hm/cs/katz/swt2/agenda/service/ValidationService.java
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/service/ValidationService.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class ValidationService {
 
   private static final Logger LOG = LoggerFactory.getLogger(ValidationService.class);
-
+  private static String EINGABEHINWEIS = " Bitte passen Sie Ihre Eingabe an!";
 
   // Check Topic title, shortDescription, longDescription requirements
   public static void topicValidation(String title, String shortDescription,
@@ -16,12 +16,12 @@ public class ValidationService {
 
     if (title.length() < 10) {
       LOG.debug("Titel von Topics müssen mindestens 10 Zeichen lang sein!");
-      throw new ValidationException("Titel müssen mindestens 10 Zeichen lang sein!");
+      throw new ValidationException("Titel müssen mindestens 10 Zeichen lang sein!" + EINGABEHINWEIS);
     }
 
     if (title.length() > 60) {
       LOG.debug("Titel von Topics dürfen nicht länger als 60 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 60 Zeichen überschritten!");
+      throw new ValidationException("Maximale Länge von 60 Zeichen überschritten!" + EINGABEHINWEIS);
     }
     topicValidation(shortDescription, longDescription);
 
@@ -32,21 +32,21 @@ public class ValidationService {
 
     if (shortDescription.length() < 100) {
       LOG.debug("Kurzbeschreibungen von Topics müssen mindestens 100 Zeichen lang sein!");
-      throw new ValidationException("Kurzbeschreibungen müssen mindestens 100 Zeichen lang sein!");
+      throw new ValidationException("Kurzbeschreibungen müssen mindestens 100 Zeichen lang sein!" + EINGABEHINWEIS);
     }
 
     if (shortDescription.length() > 200) {
       LOG.debug("Kurzbeschreibungen von Topics dürfen nicht länger als 200 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 200 Zeichen überschritten!");
+      throw new ValidationException("Maximale Länge von 200 Zeichen überschritten!" + EINGABEHINWEIS);
     }
 
     if (longDescription.length() < 200) {
       LOG.debug("Langbeschreibungen von Topics müssen mindestens 200 Zeichen lang sein!");
-      throw new ValidationException("Langbeschreibungen müssen mindestens 200 Zeichen lang sein!");
+      throw new ValidationException("Langbeschreibungen müssen mindestens 200 Zeichen lang sein!" + EINGABEHINWEIS);
     }
     if (longDescription.length() > 2000) {
       LOG.debug("Langbeschreibungen von Topics dürfen nicht länger als 2000 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 2000 Zeichen überschritten!");
+      throw new ValidationException("Maximale Länge von 2000 Zeichen überschritten!" + EINGABEHINWEIS);
     }
   }
 
@@ -56,11 +56,11 @@ public class ValidationService {
 
     if (titel.length() < 8) {
       LOG.debug("Titel von Tasks müssen mindestens 8 Zeichen lang sein!");
-      throw new ValidationException("Titel müssen mindestens 8 Zeichen lang sein!");
+      throw new ValidationException("Titel müssen mindestens 8 Zeichen lang sein!" + EINGABEHINWEIS);
     }
     if (titel.length() > 32) {
       LOG.debug("Titel von Tasks dürfen nicht länger als 32 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 32 Zeichen überschritten!");
+      throw new ValidationException("Maximale Länge von 32 Zeichen überschritten!" + EINGABEHINWEIS);
     }
     taskValidation(taskShortDescription, taskLongDescription);
 
@@ -71,20 +71,20 @@ public class ValidationService {
 
     if (taskShortDescription.length() < 100) {
       LOG.debug("Kurzbeschreibungen von Tasks müssen mindestens 100 Zeichen lang sein!");
-      throw new ValidationException("Kurzbeschreibungen müssen mindestens 100 Zeichen lang sein!");
+      throw new ValidationException("Kurzbeschreibungen müssen mindestens 100 Zeichen lang sein!" + EINGABEHINWEIS);
     }
     if (taskShortDescription.length() > 200) {
       LOG.debug("Kurzbeschreibungen von Tasks dürfen nicht länger als 200 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 200 Zeichen ueberschritten!");
+      throw new ValidationException("Maximale Länge von 200 Zeichen ueberschritten!" + EINGABEHINWEIS);
     }
 
     if (taskLongDescription.length() < 200) {
       LOG.debug("Langbeschreibungen von Tasks müssen mindestens 200 Zeichen lang sein!");
-      throw new ValidationException("Langbeschreibungen müssen mindestens 200 Zeichen lang sein!");
+      throw new ValidationException("Langbeschreibungen müssen mindestens 200 Zeichen lang sein!" + EINGABEHINWEIS);
     }
     if (taskLongDescription.length() > 2000) {
       LOG.debug("Langbeschreibungen von Tasks dürfen nicht länger als 2000 Zeichen sein!");
-      throw new ValidationException("Maximale Länge von 2000 Zeichen überschritten!");
+      throw new ValidationException("Maximale Länge von 2000 Zeichen überschritten!" + EINGABEHINWEIS);
     }
   }
   
@@ -92,41 +92,30 @@ public class ValidationService {
   public static void userValidation(String login, String name, String password) {
 
     if (!login.equals(login.toLowerCase())) {
-      LOG.debug("Name eines Users darf nur aus kleingeschriebenen Buchstaben bestehen. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Name des Anwenders darf nur aus kleingeschriebenen Buchstaben bestehen. "
-              + "Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Der Name eines Users darf nur aus kleingeschriebenen Buchstaben bestehen!");
+      throw new ValidationException("Der Name darf nur aus kleingeschriebenen Buchstaben bestehen!" + EINGABEHINWEIS);
     }
 
     if (login.length() < 4 || login.length() > 20) {
-      LOG.debug("Name eines Users muss zwischen 4 und 20 Zeichen lang sein. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Der Name muss zwischen 4 und 20 Zeichen lang sein. Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Der Name eines Users muss zwischen 4 und 20 Zeichen lang sein!");
+      throw new ValidationException("Der Name muss zwischen 4 und 20 Zeichen lang sein!" + EINGABEHINWEIS);
     }
 
     if (name.length() < 1 || name.length() > 32) {
-      LOG.debug("Name eines Users muss zwischen 1 und 32 Zeichen lang sein."
-          + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Der Name muss zwischen 1 und 32 Zeichen lang sein. Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Der Name eines Users muss zwischen 1 und 32 Zeichen lang sein!");
+      throw new ValidationException("Der Name muss zwischen 1 und 32 Zeichen lang sein!" + EINGABEHINWEIS);
     }
 
     if (password.length() < 8 || password.length() > 20) {
-      LOG.debug("Das Passwort eines Users muss zwischen 8 und 20 Zeichen lang sein. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException("Das Passwort muss zwischen 8 und 20 Zeichen lang sein. "
-          + "Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users muss zwischen 8 und 20 Zeichen lang sein!");
+      throw new ValidationException("Das Passwort muss zwischen 8 und 20 Zeichen lang sein!" + EINGABEHINWEIS);
     }
 
     String leerzeichen = " ";
 
     if (password.contains(leerzeichen)) {
-      LOG.debug("Das Passwort eines Users darf keine Leerzeichen beinhlaten."
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Das Passwort darf keine Leerzeichen beinhlaten. Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users darf keine Leerzeichen beinhlaten!");
+      throw new ValidationException("Das Passwort darf keine Leerzeichen beinhlaten!" + EINGABEHINWEIS);
     }
 
     Pattern pruefenAufGrossbuchstaben = Pattern.compile("[A-Z]");
@@ -135,37 +124,26 @@ public class ValidationService {
 
 
     if (!pruefenAufGrossbuchstaben.matcher(password).find()) {
-      LOG.debug("Das Passwort eines Users muss mindestens einen Großbuchstaben beinhalten. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Das Passwort muss mindestens einen Großbuchstaben beinhalten. "
-              + "Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users muss mindestens einen Großbuchstaben beinhalten!");
+      throw new ValidationException("Das Passwort muss mindestens einen Großbuchstaben beinhalten!" + EINGABEHINWEIS);
     }
 
     if (!pruefenAufKleinbuchstaben.matcher(password).find()) {
-      LOG.debug("Das Passwort eines Users muss mindestens einen Kleinbuchstaben beinhalten. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Das Passwort muss mindestens einen Kleinbuchstaben beinhalten. "
-              + "Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users muss mindestens einen Kleinbuchstaben beinhalten!");
+      throw new ValidationException("Das Passwort muss mindestens einen Kleinbuchstaben beinhalten!" + EINGABEHINWEIS);
     }
 
     if (!pruefenAufZahl.matcher(password).find()) {
-      LOG.debug("Das Passwort eines Users muss mindestens eine Zahl beinhalten."
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Das Passwort muss mindestens eine Zahl beinhalten. Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users muss mindestens eine Zahl beinhalten!");
+      throw new ValidationException("Das Passwort muss mindestens eine Zahl beinhalten!" + EINGABEHINWEIS);
 
     }
 
     Pattern pruefenAufSonderzeichen = Pattern.compile("[*?!$%&]");
 
     if (!pruefenAufSonderzeichen.matcher(password).find()) {
-      LOG.debug("Das Passwort eines Users muss mindestens eines dieser Sonderzeichen beinhalten: *?!$%&. "
-              + "Bitte passen Sie Ihre Eingabe an!");
-      throw new ValidationException(
-          "Das Passwort muss mindestens eines dieser Sonderzeichen beinhalten: *?!$%&. "
-              + "Bitte passen Sie Ihre Eingabe an!");
+      LOG.debug("Das Passwort eines Users muss mindestens eines dieser Sonderzeichen beinhalten: *?$%&.!");
+      throw new ValidationException("Das Passwort muss mindestens eines dieser Sonderzeichen beinhalten: *?$%&.!" + EINGABEHINWEIS);
     }
   }
   
